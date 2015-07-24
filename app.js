62559e3261703e67aca7de00f0d7d397413a179f
Tim <tklein@outlook.com>
2015-07-24 09:39:10 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var indexController = require('./controllers/index.js');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', indexController.index);
+
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
